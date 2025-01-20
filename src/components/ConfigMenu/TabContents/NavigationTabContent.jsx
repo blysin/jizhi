2b3a559e5cbe9086a496fa931ff6b1d0d14ba2ab
daishaokun1 <daishaokun1@leedarson.com>
2025-01-20 16:40:19 +0800

    修改样式

--- src/components/ConfigMenu/TabContents/NavigationTabContent.jsx
+++ src/components/ConfigMenu/TabContents/NavigationTabContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { TextInput, Button, Pane } from 'evergreen-ui';
+import { TextInput, Button, Pane, Menu } from 'evergreen-ui';
 import Storager from '../../../utils/storager';
 
 const NavigationTabContent = ({ onLinksChange }) => {
@@ -29,24 +29,35 @@ const NavigationTabContent = ({ onLinksChange }) => {
   };
 
   return (
-    <Pane display="flex" flexDirection="column" gap={8} padding={16}>
-      <TextInput
-        name="name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="网站名称"
-      />
-      <TextInput
-        name="url"
-        value={url}
-        onChange={e => setUrl(e.target.value)}
-        placeholder="网站地址"
-        type="url"
-      />
-      <Button appearance="primary" onClick={handleAddLink} width={80}>
-        添加
-      </Button>
-    </Pane>
+    <>
+      <Menu.Group title="行列配置">
+        <Menu.Option>行列配置</Menu.Option>
+        <Menu.Option>行列配置</Menu.Option>
+
+      </Menu.Group>
+      <Menu.Divider />
+
+      <Menu.Group title="添加导航">
+        <Pane display="flex" flexDirection="column" gap={8} padding={16}>
+          <TextInput
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="网站名称"
+          />
+          <TextInput
+            name="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="网站地址"
+            type="url"
+          />
+          <Button appearance="primary" onClick={handleAddLink} width={80}>
+            添加
+          </Button>
+        </Pane>
+      </Menu.Group>
+    </>
   );
 };
 
